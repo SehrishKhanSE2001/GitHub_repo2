928d7adb8a8326a5405e0db1c5e02f626cb23e1d
Sehrish Khan <i200817@nu.edu.pk>
2024-02-19 09:54:34 +0500

    commit

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import pytest
 from main import StudentsInMLOps
-
 def test_enroll_students():
     mlops_class = StudentsInMLOps()
     mlops_class.enrollStudents(5)
@@ -24,3 +23,5 @@ def test_get_class_name():
 
 if __name__ == "__main__":
     pytest.main()
+
+#--
